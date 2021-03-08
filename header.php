a475b99613b61886c0e6c9d8efe52230f599f12c
Matthieu Mota <matthieumota@gmail.com>
2021-03-08 16:32:01 +0100

    Gitignore, assets, npm (fontawesome)

--- header.php
+++ header.php
@@ -10,11 +10,14 @@
             text-align: center;
         }
     </style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="node_modules/@fortawesome/fontawesome-free/css/all.min.css">
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body class="background cocorico les conflits !!">
     <header>
-        <div>conflit avec le header</div>    
+        <div>conflit avec le header</div>  
+        <i class="fas fa-air-freshener"></i>
     </header>
 </body>
 </html>
