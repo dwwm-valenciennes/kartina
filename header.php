7e765a213978b4557b3d8e2e8cf266c7cd3aeafa
samuel <samuel.anneheim@outlook.fr>
2021-03-08 15:30:36 +0100

    modification du header

--- header.php
+++ header.php
@@ -1,14 +1,19 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Header</title>
+    <style>
+        div{
+            text-align: center;
+        }
+    </style>
 </head>
-<body>
+<body class="les conflits !!">
     <head>
-        <div>HEADER</div>    
+        <div>conflit</div>    
     </head>
 </body>
 </html>
