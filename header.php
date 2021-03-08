14fb783929fd5d3ee462335c3f4b374e1993ad98
merlin <ameye.melissa@gmail.com>
2021-03-08 15:30:38 +0100

    super conflit

--- header.php
+++ header.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="de">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Header</title>
+    <style></style>
+    <link rel="stylesheet" href="#">
 </head>
-<body>
-    <head>
+<body class="cocorico">
+    <header>
         <div>HEADER</div>    
-    </head>
+    </header>
 </body>
 </html>
